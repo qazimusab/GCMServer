f1813459d91938e6486a80a6db54dbaea44c6107
qaziahmed <qazi.ahmed@ncr.com>
2015-12-03 23:47:39 -0500

    make it better

--- GCMWebService.py
+++ GCMWebService.py
@@ -1,27 +1,34 @@
+import sqlite3
 from flask import Flask
-from flask import jsonify, request, abort
+from flask import jsonify
+from flask import g
 
 app = Flask(__name__)
 
+DATABASE = 'database.db'
 names = ["Habib", "Okanla"]
 apiKey = "APA91bHARLcYAZ-zHZUL99bSEdpVbigGRH7TSn9CH44ov8ss6O8FKC9D6O58s3zNf4YwQmkVWLinq2dTv6hYB1ZgrWfNJUBupvuRzu_c7DX0949t7J-Ls1LWaZ97fuyLtWzK1JnvhuGU"
 projectNumber = "933940496191"
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World Jason!!'
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = g.db = connect_db()
+    return db
+
+@app.before_request
+def before_request():
+    g.db = get_db()
 
 
-@app.route('/get_names', methods=['GET'])
-def get_tasks():
-    return jsonify({'names': names})
-
-
-@app.route('/get_api_key', methods=['GET'])
-def get_api_key():
-    return jsonify({'apiKey': apiKey})
-
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        get_db().close()
 
 
 @app.route('/get_project_number', methods=['GET'])
@@ -29,12 +36,15 @@ def get_project_number():
     return jsonify({'projectNumber': projectNumber})
 
 
-# #Add 2 numbers passed as json.
-@app.route('/add', methods=['POST'])
-def add_nums():
-    if not request.json or not 'num1' in request.json or not 'num2' in request.json:
-        abort(400)
-    return jsonify({'result': request.json['num1'] + request.json['num2']}), 201
+@app.route("/device_registration_id/<device_id>", methods=['POST'])
+def post_dev_id(device_id):
+    cursor = get_db().cursor()
+    query = "CREATE TABLE IF NOT EXISTS devices (id VARCHAR(500) NOT NULL PRIMARY KEY);"
+    cursor.execute(query)
+    query = "INSERT INTO devices (id) VALUES ('" + device_id + "');"
+    cursor.execute(query)
+    cursor.close()
+    get_db().commit()
 
 
 if __name__ == '__main__':
